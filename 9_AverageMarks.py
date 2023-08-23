69372bcce60530b72fe6cd6ce2fa007f2e9814fd
jagdeep222422 <76045872+jagdeep222422@users.noreply.github.com>
2023-08-23 15:52:38 +0530

    Create 9_AverageMarks.py

---
+++ 9_AverageMarks.py
@@ -0,0 +1,9 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def averageMarks(firstLetterOfName, m1, m2, m3):
+    # Write your code here
+    # Return a pair<char, int> denoting the answer
+    return (firstLetterOfName,(m1+m2+m3)//3)
