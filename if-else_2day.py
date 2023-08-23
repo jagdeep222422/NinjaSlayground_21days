c0a233200a31a4a1360b403fcbea34994144908c
jagdeep222422 <76045872+jagdeep222422@users.noreply.github.com>
2023-08-23 15:25:21 +0530

    Create if-else_2day.py

---
+++ if-else_2day.py
@@ -0,0 +1,10 @@
+from typing import *
+
+def compareIfElse(a: int, b: int):
+    # Write your code here
+    if(a>b):
+        return "greater"
+    elif (a==b ):
+        return "equal" 
+    else :
+        return "smaller"      
