c86a4e947774fc1c4730a7ac78c7ab36a9ff6e0c
jagdeep222422 <76045872+jagdeep222422@users.noreply.github.com>
2023-08-26 14:13:13 +0530

    Create 14_printName.py

---
+++ 14_printName.py
@@ -0,0 +1,11 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def printName(s):
+    # Write your code here
+    count = 0
+    while count < 5:
+        print(s)
+        count = count+1
